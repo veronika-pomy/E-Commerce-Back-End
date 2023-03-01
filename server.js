3f8715b9abd3dbc20e365eacbf54eab0545c886a
Veronika Pomyateeva <pomyateevaav@gmail.com>
2023-02-28 20:29:20 -0500

    added product routes

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
-
+sequelize = require('./config/connection');
+ 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listeningon port ${PORT}`));
 });
+
